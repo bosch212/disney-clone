3298dfeb004351e6189711d9b86d9a04f8e8b03c
bosch212 <nasrulaytam@gmail.com>
2022-01-21 01:11:02 +0700

    sign page part 1

---
+++ src/app/App.js
@@ -0,0 +1,20 @@
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom/cjs/react-router-dom.min';
+import Login from '../components/Login';
+import Header from '../components/Header';
+import "../App.css"
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Header/>
+        <Switch>
+          <Route exact path="/">
+            <Login />
+          </Route>
+        </Switch>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
